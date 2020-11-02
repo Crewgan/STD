3506a9584da239f56990fa59844ad2f907959579
Crewgan <joey.mrtg@eduge.ch>
2020-11-02 11:32:09 +0100

    Default sort program

---
+++ DictionnaryGenerator/DictionnaryGenerator/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DictionnaryGenerator
+{
+    class Program
+    {
+
+        static int nbDictionaries = 3;
+        static char[] alphabet = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(CreateDictionaires(nbDictionaries, "Dict", ""));
+        }
+
+        public static string CreateDictionaires(int nb, string dictName, string tab)
+        {
+            //static Dictionary<char, Dictionary> " + 
+            string result = "";
+            string thisDictName;
+            for (int j = 0; j < alphabet.Length; j++)
+            {
+                thisDictName = tab + dictName + alphabet[j];
+                result += Environment.NewLine + thisDictName + " = {";
+                nb--;
+                for (int i = 0; i < nb; i++)
+                {
+                    if (nb > 0)
+                    {
+                        tab += "\t";
+                        result += tab + CreateDictionaires(nb, thisDictName, tab) + " };";
+                    }
+                    else
+                    {
+                        result += "finish";
+                    }
+                }
+            }
+            return result;
+        }
+    }
+}
---
+++ FileSort/FileSort/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace FileSort
+{
+    class Program
+    {
+        static string newFilePath = AppDomain.CurrentDomain.BaseDirectory + @"\SortedFile.txt";
+        static string FilePath = AppDomain.CurrentDomain.BaseDirectory + @"\TempFile.txt";
+        static Char[] charsToReplace = { ' ' };
+        static Char[] CharsToDelete = { '.', ',', '?', '!', '[', ']', '{', '}', '(', ')', '"', '\'', ' ' };
+        static Stopwatch sp = new Stopwatch();
+        //static Dictionary<char, Dictionary<char, List<string>>> Dict = new Dictionary<char, Dictionary<char, List<string>>>{ { 'a', new Dictionary<char, List<string>>{ 'a', new List<string>() {"yo"} } } };
+
+        static void Main(string[] args)
+        {
+            sp.Start();
+            using (var writer = new StreamWriter(File.CreateText(newFilePath).BaseStream))
+            {
+                using (var reader = new StreamReader(FilePath))
+                {
+                    Console.SetOut(writer);
+                    Console.SetIn(reader);
+                    string line;
+                    string[] splitContent;
+                    line = reader.ReadToEnd();
+
+                    string content = new string(line.Select(c => charsToReplace.Contains(c) ? '\n' : CharsToDelete.Contains(c) ? '\0' : c ).ToArray()); // Remove or replace characters with \n when necessary
+
+                    splitContent = content.Split('\n');
+                    splitContent = splitContent.OrderBy(c => c).ToArray();
+                    Console.WriteLine(String.Join("\n", splitContent));
+                    sp.Stop();
+                    Console.WriteLine(string.Format("Time to sort {0} ms", sp.ElapsedMilliseconds));
+                }
+            }
+            
+        }
+    }
+}
